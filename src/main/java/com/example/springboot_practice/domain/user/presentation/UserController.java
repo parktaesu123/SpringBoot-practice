c89edab318b433ec52bed6a2a1f4ea9fb3902a70
troy96475 <taesupark0504@gmail.com>
2025-01-16 21:29:08 +0900

    feat :: deleteUser service

--- src/main/java/com/example/springboot_practice/domain/user/presentation/UserController.java
+++ src/main/java/com/example/springboot_practice/domain/user/presentation/UserController.java
@@ -1,20 +1,25 @@
 package com.example.springboot_practice.domain.user.presentation;
 
+import com.example.springboot_practice.domain.user.application.DeleteUserService;
 import com.example.springboot_practice.domain.user.application.MyInfoService;
 import com.example.springboot_practice.domain.user.presentation.dto.response.MyInfoResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
     private final MyInfoService myInfoService;
+    private final DeleteUserService deleteUserService;
 
     @GetMapping("/my-info")
     public MyInfoResponse myInfo() {
         return myInfoService.myInfo();
+    }
+
+    @DeleteMapping("/{accountId}")
+    public void deleteUser(@PathVariable String accountId) {
+        deleteUserService.deleteUser(accountId);
     }
 }
