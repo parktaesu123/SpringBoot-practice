2a9d1c76e67d90d047ad445344fef7590d299f16
troy96475 <taesupark0504@gmail.com>
2025-01-16 21:09:36 +0900

    refactor :: logout 기능

--- src/main/java/com/example/springboot_practice/domain/auth/application/LogoutService.java
+++ src/main/java/com/example/springboot_practice/domain/auth/application/LogoutService.java
@@ -1,20 +1,26 @@
 package com.example.springboot_practice.domain.auth.application;
 
+import com.example.springboot_practice.domain.auth.domain.RefreshToken;
+import com.example.springboot_practice.domain.auth.domain.repository.RefreshTokenRepository;
+import com.example.springboot_practice.domain.auth.exception.RefreshTokenNotFoundException;
 import com.example.springboot_practice.domain.user.application.facade.UserFacade;
 import com.example.springboot_practice.domain.user.domain.User;
-import com.example.springboot_practice.domain.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogoutService {
-    private final UserRepository userRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
     private final UserFacade userFacade;
 
     public void logout() {
         User user = userFacade.currentUser();
 
+        RefreshToken refreshToken = refreshTokenRepository.findById(user.getAccountId())
+                .orElseThrow(() -> RefreshTokenNotFoundException.EXCEPTION);
+
+        refreshTokenRepository.delete(refreshToken);
 
     }
 }
