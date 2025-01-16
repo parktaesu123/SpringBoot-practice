b835debb3d0ec3ce805a6cdbd63a19f1c551aa3b
troy96475 <taesupark0504@gmail.com>
2025-01-16 21:29:20 +0900

    refactor :: deleteUserService

--- src/main/java/com/example/springboot_practice/domain/user/application/DeleteUserService.java
+++ src/main/java/com/example/springboot_practice/domain/user/application/DeleteUserService.java
@@ -1,2 +1,22 @@
-package com.example.springboot_practice.domain.user.application;public class DeleteUserService {
+package com.example.springboot_practice.domain.user.application;
+
+import com.example.springboot_practice.domain.user.application.facade.UserFacade;
+import com.example.springboot_practice.domain.user.domain.User;
+import com.example.springboot_practice.domain.user.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class DeleteUserService {
+    private final UserFacade userFacade;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void deleteUser(String accountId) {
+        User user = userFacade.currentUser();
+
+        userRepository.deleteByAccountId(accountId);
+    }
 }
