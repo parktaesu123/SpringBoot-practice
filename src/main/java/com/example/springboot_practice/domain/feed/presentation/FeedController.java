f85c25ab1f0602b8aed319266a92e7aa2798acb9
troy96475 <taesupark0504@gmail.com>
2025-01-18 02:36:48 +0900

    feat :: 글 수정 api

--- src/main/java/com/example/springboot_practice/domain/feed/presentation/FeedController.java
+++ src/main/java/com/example/springboot_practice/domain/feed/presentation/FeedController.java
@@ -3,6 +3,7 @@ package com.example.springboot_practice.domain.feed.presentation;
 import com.example.springboot_practice.domain.feed.application.CreateFeedService;
 import com.example.springboot_practice.domain.feed.application.QueryAllFeedService;
 import com.example.springboot_practice.domain.feed.application.QueryMyFeedService;
+import com.example.springboot_practice.domain.feed.application.UpdateFeedService;
 import com.example.springboot_practice.domain.feed.presentation.dto.request.FeedRequest;
 import com.example.springboot_practice.domain.feed.presentation.dto.response.FeedListResponse;
 import jakarta.validation.Valid;
@@ -18,6 +19,7 @@ public class FeedController {
     private final CreateFeedService createFeedService;
     private final QueryMyFeedService queryMyFeedService;
     private final QueryAllFeedService queryAllFeedService;
+    private final UpdateFeedService updateFeedService;
 
 
     @PostMapping("")
@@ -34,4 +36,10 @@ public class FeedController {
     public List<FeedListResponse> queryAllFeed() {
         return queryAllFeedService.queryAllFeed();
     }
+
+    @PatchMapping("/{title}")
+    public void updateFeed(@PathVariable String title, @RequestBody @Valid FeedRequest request) {
+        updateFeedService.updateFeed(title, request);
+    }
 }
+
