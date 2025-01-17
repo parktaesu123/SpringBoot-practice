1e5184fbde7765d6f42ebfdf0a8b25bded981c8b
troy96475 <taesupark0504@gmail.com>
2025-01-17 22:48:24 +0900

    feat :: 내 글 조회 dao

--- src/main/java/com/example/springboot_practice/domain/feed/domain/repository/FeedRepositoryCustom.java
+++ src/main/java/com/example/springboot_practice/domain/feed/domain/repository/FeedRepositoryCustom.java
@@ -1,4 +1,9 @@
 package com.example.springboot_practice.domain.feed.domain.repository;
 
+import com.example.springboot_practice.domain.feed.presentation.dto.response.FeedListResponse;
+
+import java.util.List;
+
 public interface FeedRepositoryCustom {
+    List<FeedListResponse> findFeedByAccountId(String accountId);
 }
--- src/main/java/com/example/springboot_practice/domain/feed/domain/repository/FeedRepositoryCustomImpl.java
+++ src/main/java/com/example/springboot_practice/domain/feed/domain/repository/FeedRepositoryCustomImpl.java
@@ -1,4 +1,28 @@
 package com.example.springboot_practice.domain.feed.domain.repository;
 
+import com.example.springboot_practice.domain.feed.domain.QFeed;
+import com.example.springboot_practice.domain.feed.presentation.dto.response.FeedListResponse;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
 public class FeedRepositoryCustomImpl implements FeedRepositoryCustom {
+    private final JPAQueryFactory jpaQueryFactory;
+    private final QFeed qFeed = QFeed.feed;
+
+    @Override
+    public List<FeedListResponse> findFeedByAccountId(String accountId) {
+        return jpaQueryFactory.select(Projections.constructor(FeedListResponse.class, qFeed.title, qFeed.content, qFeed.createdAt))
+                .from(qFeed)
+                .where(qFeed.user.accountId.eq(accountId))
+                .groupBy(qFeed.id)
+                .orderBy(qFeed.createdAt.desc())
+                .fetch();
+
+    }
+
+
 }
