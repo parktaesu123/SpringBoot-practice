52d8ce4279d221510f631e356c3a617e54d8cb17
troy96475 <taesupark0504@gmail.com>
2025-01-18 02:36:21 +0900

    feat :: findByTitle

--- src/main/java/com/example/springboot_practice/domain/feed/domain/repository/FeedRepository.java
+++ src/main/java/com/example/springboot_practice/domain/feed/domain/repository/FeedRepository.java
@@ -4,6 +4,9 @@ import com.example.springboot_practice.domain.feed.domain.Feed;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FeedRepository extends CrudRepository<Feed, Long>, FeedRepositoryCustom {
+    Optional<Feed> findByTitle(String title);
 }
