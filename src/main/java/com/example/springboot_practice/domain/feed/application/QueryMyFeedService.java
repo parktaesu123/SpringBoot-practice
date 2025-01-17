594155335b1c5a6dfcd17eb2237a981b2168b061
troy96475 <taesupark0504@gmail.com>
2025-01-17 22:48:50 +0900

    feat :: 내 글 조회 서비스

--- src/main/java/com/example/springboot_practice/domain/feed/application/QueryMyFeedService.java
+++ src/main/java/com/example/springboot_practice/domain/feed/application/QueryMyFeedService.java
@@ -1,2 +1,25 @@
-package com.example.springboot_practice.domain.feed.application;public class QueryMyFeedService {
+package com.example.springboot_practice.domain.feed.application;
+
+import com.example.springboot_practice.domain.feed.domain.repository.FeedRepository;
+import com.example.springboot_practice.domain.feed.presentation.dto.response.FeedListResponse;
+import com.example.springboot_practice.domain.user.application.facade.UserFacade;
+import com.example.springboot_practice.domain.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class QueryMyFeedService {
+    private final FeedRepository feedRepository;
+    private final UserFacade userFacade;
+
+    @Transactional(readOnly = true)
+    public List<FeedListResponse> queryMyFeed() {
+        User user = userFacade.currentUser();
+
+        return feedRepository.findFeedByAccountId(user.getAccountId());
+    }
 }
