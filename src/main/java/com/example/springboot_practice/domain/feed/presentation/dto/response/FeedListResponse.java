ab9ab38f641d8a8bdfab7e4aacef457cbf317325
troy96475 <taesupark0504@gmail.com>
2025-01-17 22:49:10 +0900

    feat :: 내 글 조회 dto

--- src/main/java/com/example/springboot_practice/domain/feed/presentation/dto/response/FeedListResponse.java
+++ src/main/java/com/example/springboot_practice/domain/feed/presentation/dto/response/FeedListResponse.java
@@ -1,2 +1,12 @@
-package com.example.springboot_practice.domain.feed.presentation.dto.response;public class FeedListResponse {
+package com.example.springboot_practice.domain.feed.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class FeedListResponse {
+    private String title;
+    private String content;
+    private String createdAt;
 }
