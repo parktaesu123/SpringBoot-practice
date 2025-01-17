40e6883c53d3c03eef4ba00b9d9e11602440a2d9
troy96475 <taesupark0504@gmail.com>
2025-01-17 22:49:28 +0900

    feat :: 내 글 조회 로직 추가

--- src/main/java/com/example/springboot_practice/domain/feed/presentation/FeedController.java
+++ src/main/java/com/example/springboot_practice/domain/feed/presentation/FeedController.java
@@ -1,23 +1,30 @@
 package com.example.springboot_practice.domain.feed.presentation;
 
 import com.example.springboot_practice.domain.feed.application.CreateFeedService;
+import com.example.springboot_practice.domain.feed.application.QueryMyFeedService;
 import com.example.springboot_practice.domain.feed.presentation.dto.request.FeedRequest;
+import com.example.springboot_practice.domain.feed.presentation.dto.response.FeedListResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/feed")
 @RequiredArgsConstructor
 public class FeedController {
     private final CreateFeedService createFeedService;
+    private final QueryMyFeedService queryMyFeedService;
 
 
     @PostMapping("")
     public void createFeed(@RequestBody @Valid FeedRequest request) {
         createFeedService.createFeed(request);
+    }
+
+    @GetMapping("")
+    public List<FeedListResponse> queryMyFeed() {
+        return queryMyFeedService.queryMyFeed();
     }
 }
