a77511c3e3e8a6533d0736947a5c68d55a8b7c27
troy96475 <taesupark0504@gmail.com>
2025-01-18 19:09:19 +0900

    feat :: 댓글 도메인

---
+++ src/main/java/com/example/springboot_practice/domain/comment/domain/Comment.java
@@ -0,0 +1,35 @@
+package com.example.springboot_practice.domain.comment.domain;
+
+import com.example.springboot_practice.domain.feed.domain.Feed;
+import com.example.springboot_practice.domain.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    private String createdAt;
+
+    private String updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "feed_id", nullable = false)
+    private Feed feed;
+}
