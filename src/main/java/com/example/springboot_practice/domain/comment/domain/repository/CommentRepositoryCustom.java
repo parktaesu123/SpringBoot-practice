5df337a4b1943723e072f42a09218a093e823ee3
troy96475 <taesupark0504@gmail.com>
2025-01-18 22:43:17 +0900

    feat :: query추가

--- src/main/java/com/example/springboot_practice/domain/comment/domain/repository/CommentRepositoryCustom.java
+++ src/main/java/com/example/springboot_practice/domain/comment/domain/repository/CommentRepositoryCustom.java
@@ -1,9 +1,12 @@
 package com.example.springboot_practice.domain.comment.domain.repository;
 
+import com.example.springboot_practice.domain.comment.domain.presentation.dto.response.CommentDetailResponse;
 import com.example.springboot_practice.domain.comment.domain.presentation.dto.response.CommentListResponse;
 
 import java.util.List;
 
 public interface CommentRepositoryCustom {
     List<CommentListResponse> findCommentByUserName(String userName);
+
+    List<CommentDetailResponse> findCommentByTitle(String title);
 }
--- src/main/java/com/example/springboot_practice/domain/comment/domain/repository/CommentRepositoryCustomImpl.java
+++ src/main/java/com/example/springboot_practice/domain/comment/domain/repository/CommentRepositoryCustomImpl.java
@@ -1,6 +1,7 @@
 package com.example.springboot_practice.domain.comment.domain.repository;
 
 import com.example.springboot_practice.domain.comment.domain.QComment;
+import com.example.springboot_practice.domain.comment.domain.presentation.dto.response.CommentDetailResponse;
 import com.example.springboot_practice.domain.comment.domain.presentation.dto.response.CommentListResponse;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -18,6 +19,16 @@ public class CommentRepositoryCustomImpl implements CommentRepositoryCustom {
         return jpaQueryFactory.select(Projections.constructor(CommentListResponse.class, qComment.content, qComment.createdAt, qComment.user.userName, qComment.feed.title))
                 .from(qComment)
                 .where(qComment.user.userName.eq(userName))
+                .groupBy(qComment.id)
+                .orderBy(qComment.createdAt.desc())
+                .fetch();
+    }
+
+    @Override
+    public List<CommentDetailResponse> findCommentByTitle(String title) {
+        return jpaQueryFactory.select(Projections.constructor(CommentDetailResponse.class, qComment.content, qComment.createdAt, qComment.user.userName, qComment.feed.title))
+                .from(qComment)
+                .where(qComment.feed.title.eq(title))
                 .groupBy(qComment.id)
                 .orderBy(qComment.createdAt.desc())
                 .fetch();
