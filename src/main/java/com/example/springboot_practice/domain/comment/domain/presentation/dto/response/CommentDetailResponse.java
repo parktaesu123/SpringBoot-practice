fdc7c05cb28adab20f970c64e5d48d8611acca50
troy96475 <taesupark0504@gmail.com>
2025-01-18 22:43:07 +0900

    feat :: 제목으로 댓글 조회

--- src/main/java/com/example/springboot_practice/domain/comment/domain/presentation/dto/response/CommentDetailResponse.java
+++ src/main/java/com/example/springboot_practice/domain/comment/domain/presentation/dto/response/CommentDetailResponse.java
@@ -1,2 +1,13 @@
-package com.example.springboot_practice.domain.comment.domain.presentation.dto.response;public class CommentDetailResponse {
+package com.example.springboot_practice.domain.comment.domain.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CommentDetailResponse {
+    private String content;
+    private String createdAt;
+    private String userName;
+    private String title;
 }
