b7284ee922ca3c149a52c253d5dfde57ab67453c
troy96475 <taesupark0504@gmail.com>
2025-01-18 20:25:32 +0900

    feat :: 댓글 조회 query

--- src/main/java/com/example/springboot_practice/domain/comment/domain/repository/CommentRepositoryCustom.java
+++ src/main/java/com/example/springboot_practice/domain/comment/domain/repository/CommentRepositoryCustom.java
@@ -1,4 +1,9 @@
 package com.example.springboot_practice.domain.comment.domain.repository;
 
+import com.example.springboot_practice.domain.comment.domain.presentation.dto.response.CommentListResponse;
+
+import java.util.List;
+
 public interface CommentRepositoryCustom {
+    List<CommentListResponse> findCommentByUserName(String userName);
 }
--- src/main/java/com/example/springboot_practice/domain/comment/domain/repository/CommentRepositoryCustomImpl.java
+++ src/main/java/com/example/springboot_practice/domain/comment/domain/repository/CommentRepositoryCustomImpl.java
@@ -1,4 +1,25 @@
 package com.example.springboot_practice.domain.comment.domain.repository;
 
+import com.example.springboot_practice.domain.comment.domain.QComment;
+import com.example.springboot_practice.domain.comment.domain.presentation.dto.response.CommentListResponse;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
 public class CommentRepositoryCustomImpl implements CommentRepositoryCustom {
+    private final JPAQueryFactory jpaQueryFactory;
+    private final QComment qComment = QComment.comment;
+
+    @Override
+    public List<CommentListResponse> findCommentByUserName(String userName) {
+        return jpaQueryFactory.select(Projections.constructor(CommentListResponse.class, qComment.content, qComment.createdAt, qComment.user.userName, qComment.feed.title))
+                .from(qComment)
+                .where(qComment.user.userName.eq(userName))
+                .groupBy(qComment.id)
+                .orderBy(qComment.createdAt.desc())
+                .fetch();
+    }
 }
