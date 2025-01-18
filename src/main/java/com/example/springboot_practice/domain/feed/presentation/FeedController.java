16276e60215c17ac3dcbf291ef393489b82649fa
troy96475 <taesupark0504@gmail.com>
2025-01-18 22:02:10 +0900

    feat :: 게시글 조회 api

--- src/main/java/com/example/springboot_practice/domain/feed/presentation/FeedController.java
+++ src/main/java/com/example/springboot_practice/domain/feed/presentation/FeedController.java
@@ -3,6 +3,7 @@ package com.example.springboot_practice.domain.feed.presentation;
 import com.example.springboot_practice.domain.feed.application.*;
 import com.example.springboot_practice.domain.feed.presentation.dto.request.FeedRequest;
 import com.example.springboot_practice.domain.feed.presentation.dto.response.FeedListResponse;
+import com.example.springboot_practice.domain.feed.presentation.dto.response.FeedResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +19,7 @@ public class FeedController {
     private final QueryAllFeedService queryAllFeedService;
     private final UpdateFeedService updateFeedService;
     private final DeleteFeedService deleteFeedService;
+    private final QueryFeedService queryfeedService;
 
 
     @PostMapping("")
@@ -25,12 +27,12 @@ public class FeedController {
         createFeedService.createFeed(request);
     }
 
-    @GetMapping("{accountId}")
+    @GetMapping("/user/{accountId}")
     public List<FeedListResponse> queryMyFeed(@PathVariable String accountId) {
         return queryMyFeedService.queryMyFeed(accountId);
     }
 
-    @GetMapping("")
+    @GetMapping("/all")
     public List<FeedListResponse> queryAllFeed() {
         return queryAllFeedService.queryAllFeed();
     }
@@ -40,9 +42,15 @@ public class FeedController {
         updateFeedService.updateFeed(title, request);
     }
 
+    @GetMapping("/{title}")
+    public FeedResponse queryFeed(@PathVariable String title) {
+        return queryfeedService.queryFeed(title);
+    }
+
     @DeleteMapping("/{title}")
     public void deleteFeed(@PathVariable String title) {
         deleteFeedService.deleteFeed(title);
     }
+
 }
 
