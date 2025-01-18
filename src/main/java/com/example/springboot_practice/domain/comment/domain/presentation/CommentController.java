99bf347777540add7b063f6cc1904a9526edc86b
troy96475 <taesupark0504@gmail.com>
2025-01-18 22:43:23 +0900

    feat :: api

--- src/main/java/com/example/springboot_practice/domain/comment/domain/presentation/CommentController.java
+++ src/main/java/com/example/springboot_practice/domain/comment/domain/presentation/CommentController.java
@@ -2,7 +2,9 @@ package com.example.springboot_practice.domain.comment.domain.presentation;
 
 import com.example.springboot_practice.domain.comment.domain.application.CreateCommentService;
 import com.example.springboot_practice.domain.comment.domain.application.QueryCommentByUserNameService;
+import com.example.springboot_practice.domain.comment.domain.application.QueryCommentListService;
 import com.example.springboot_practice.domain.comment.domain.presentation.dto.request.CommentRequest;
+import com.example.springboot_practice.domain.comment.domain.presentation.dto.response.CommentDetailResponse;
 import com.example.springboot_practice.domain.comment.domain.presentation.dto.response.CommentListResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +18,7 @@ import java.util.List;
 public class CommentController {
     private final CreateCommentService createCommentService;
     private final QueryCommentByUserNameService queryCommentService;
+    private final QueryCommentListService queryCommentListService;
 
     @PostMapping("/{title}")
     public void createComment(@PathVariable String title, @RequestBody @Valid CommentRequest request) {
@@ -25,5 +28,10 @@ public class CommentController {
     @GetMapping("/{title}/{userName}")
     public List<CommentListResponse> queryCommentByUserName(@PathVariable String title, @PathVariable String userName) {
         return queryCommentService.queryCommentByUserName(title, userName);
+    }
+
+    @GetMapping("/{title}")
+    public List<CommentDetailResponse> queryCommentByTitle(@PathVariable String title) {
+        return queryCommentListService.queryAllComment(title);
     }
 }
