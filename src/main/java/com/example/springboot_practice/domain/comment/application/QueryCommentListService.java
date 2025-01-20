30e188cf1b8fd0c5172ae07776777e910ca1a2a8
troy96475 <taesupark0504@gmail.com>
2025-01-20 20:17:14 +0900

    feat :: 해당 게시글 댓글 조회

---
+++ src/main/java/com/example/springboot_practice/domain/comment/application/QueryCommentListService.java
@@ -0,0 +1,25 @@
+package com.example.springboot_practice.domain.comment.application;
+
+import com.example.springboot_practice.domain.comment.presentation.dto.response.CommentDetailResponse;
+import com.example.springboot_practice.domain.comment.domain.repository.CommentRepository;
+import com.example.springboot_practice.domain.user.application.facade.UserFacade;
+import com.example.springboot_practice.domain.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class QueryCommentListService {
+    private final UserFacade userFacade;
+    private final CommentRepository commentRepository;
+
+    @Transactional(readOnly = true)
+    public List<CommentDetailResponse> queryAllComment(String title) {
+        User user = userFacade.currentUser();
+
+        return commentRepository.findCommentByTitle(title);
+    }
+}
