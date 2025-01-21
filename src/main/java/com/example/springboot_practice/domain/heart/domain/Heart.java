22dff554b2904248a91c8448b26934d6785ae1d2
troy96475 <taesupark0504@gmail.com>
2025-01-21 22:37:56 +0900

    refactor :: heart 도메인 수정

--- src/main/java/com/example/springboot_practice/domain/heart/domain/Heart.java
+++ src/main/java/com/example/springboot_practice/domain/heart/domain/Heart.java
@@ -7,12 +7,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class Heart extends BaseIdEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(nullable = false)
