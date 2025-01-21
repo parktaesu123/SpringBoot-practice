7ec672f8f633b04e33cb7193f74c19584895d9f1
troy96475 <taesupark0504@gmail.com>
2025-01-21 22:39:29 +0900

    feat :: heartRepository 쿼리 메서드

--- src/main/java/com/example/springboot_practice/domain/heart/domain/repository/HeartRepository.java
+++ src/main/java/com/example/springboot_practice/domain/heart/domain/repository/HeartRepository.java
@@ -1,9 +1,13 @@
 package com.example.springboot_practice.domain.heart.domain.repository;
 
+import com.example.springboot_practice.domain.feed.domain.Feed;
 import com.example.springboot_practice.domain.heart.domain.Heart;
+import com.example.springboot_practice.domain.user.domain.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HeartRepository extends CrudRepository<Heart, Long> {
+    boolean existsHeartByUserAndFeed(User user, Feed feed);
+    void deleteHeartByUserAndFeed(User user, Feed feed);
 }
