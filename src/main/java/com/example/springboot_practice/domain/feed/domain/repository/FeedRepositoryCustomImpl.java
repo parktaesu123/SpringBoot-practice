8f8e7e1d326fea29884b2e1f3bc406192d20c497
troy96475 <taesupark0504@gmail.com>
2025-01-22 21:41:35 +0900

    refactor :: 조회 쿼리 전면 수정
    하트를 누른 유저는 글을 조회할때 heartStatus가 true로 표시 되도록 변경 (솔직히 좀 지렸다)

--- src/main/java/com/example/springboot_practice/domain/feed/domain/repository/FeedRepositoryCustomImpl.java
+++ src/main/java/com/example/springboot_practice/domain/feed/domain/repository/FeedRepositoryCustomImpl.java
@@ -3,6 +3,7 @@ package com.example.springboot_practice.domain.feed.domain.repository;
 import com.example.springboot_practice.domain.feed.domain.QFeed;
 import com.example.springboot_practice.domain.feed.presentation.dto.response.FeedListResponse;
 import com.example.springboot_practice.domain.feed.presentation.dto.response.FeedResponse;
+import com.example.springboot_practice.domain.heart.domain.QHeart;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -13,13 +14,15 @@ import java.util.List;
 public class FeedRepositoryCustomImpl implements FeedRepositoryCustom {
     private final JPAQueryFactory jpaQueryFactory;
     private final QFeed qFeed = QFeed.feed;
+    private final QHeart qHeart = QHeart.heart;
 
     @Override
     public List<FeedListResponse> findFeedByAccountId(String accountId) {
-        return jpaQueryFactory.select(Projections.constructor(FeedListResponse.class, qFeed.title, qFeed.content, qFeed.createdAt, qFeed.heartCounts, qFeed.heartStatus ,qFeed.user.userName))
+        return jpaQueryFactory.select(Projections.constructor(FeedListResponse.class, qFeed.title, qFeed.content, qFeed.createdAt, qFeed.heartCounts, qHeart.isNotNull() ,qFeed.user.userName))
                 .from(qFeed)
+                .leftJoin(qHeart)
+                .on(qHeart.user.accountId.eq(accountId))
                 .where(qFeed.user.accountId.eq(accountId))
-                .groupBy(qFeed.id)
                 .orderBy(qFeed.createdAt.desc())
                 .fetch();
 
@@ -27,24 +30,30 @@ public class FeedRepositoryCustomImpl implements FeedRepositoryCustom {
 
     @Override
     public List<FeedListResponse> findAllFeed() {
-        return jpaQueryFactory.select(Projections.constructor(FeedListResponse.class, qFeed.title, qFeed.content, qFeed.createdAt, qFeed.heartCounts, qFeed.heartStatus ,qFeed.user.userName))
+        return jpaQueryFactory.select(Projections.constructor(FeedListResponse.class, qFeed.title, qFeed.content, qFeed.createdAt, qFeed.heartCounts, qHeart.isNotNull() ,qFeed.user.userName))
                 .from(qFeed)
+                .leftJoin(qHeart)
+                .on(qHeart.feed.eq(qFeed))
                 .orderBy(qFeed.createdAt.desc())
                 .fetch();
     }
 
     @Override
     public List<FeedListResponse> findFeedByHeart() {
-        return jpaQueryFactory.select(Projections.constructor(FeedListResponse.class, qFeed.title, qFeed.content, qFeed.createdAt, qFeed.heartCounts, qFeed.heartStatus, qFeed.user.userName))
+        return jpaQueryFactory.select(Projections.constructor(FeedListResponse.class, qFeed.title, qFeed.content, qFeed.createdAt, qFeed.heartCounts, qHeart.isNotNull(), qFeed.user.userName))
                 .from(qFeed)
+                .leftJoin(qHeart)
+                .on(qHeart.feed.eq(qFeed))
                 .orderBy(qFeed.heartCounts.desc())
                 .fetch();
     }
 
     @Override
     public FeedResponse findFeedByTitle(String title) {
-        return jpaQueryFactory.select(Projections.constructor(FeedResponse.class, qFeed.title, qFeed.content, qFeed.createdAt, qFeed.heartCounts, qFeed.heartStatus, qFeed.user.userName))
+        return jpaQueryFactory.select(Projections.constructor(FeedResponse.class, qFeed.title, qFeed.content, qFeed.createdAt, qFeed.heartCounts, qHeart.isNotNull(), qFeed.user.userName))
                 .from(qFeed)
+                .leftJoin(qHeart)
+                .on(qHeart.feed.eq(qFeed))
                 .fetchOne();
     }
 
