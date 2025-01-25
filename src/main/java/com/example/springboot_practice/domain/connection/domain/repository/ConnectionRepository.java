9a8d882ca989d1007301487a0ef296223fa36537
troy96475 <taesupark0504@gmail.com>
2025-01-26 02:20:19 +0900

    feat :: 커넥션 존재 쿼리

--- src/main/java/com/example/springboot_practice/domain/connection/domain/repository/ConnectionRepository.java
+++ src/main/java/com/example/springboot_practice/domain/connection/domain/repository/ConnectionRepository.java
@@ -1,9 +1,11 @@
 package com.example.springboot_practice.domain.connection.domain.repository;
 
 import com.example.springboot_practice.domain.connection.domain.Connection;
+import com.example.springboot_practice.domain.user.domain.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ConnectionRepository extends CrudRepository<Connection, Long> {
+    boolean existsConnectionByUserAndTargetUser(User user, User targetUser);
 }
