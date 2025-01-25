159c84dc6948c70edf73d42c766c4562c5d3e1fd
troy96475 <taesupark0504@gmail.com>
2025-01-26 02:54:30 +0900

    refactor :: builder 추가

--- src/main/java/com/example/springboot_practice/domain/connection/presentation/dto/response/ConnectionResponse.java
+++ src/main/java/com/example/springboot_practice/domain/connection/presentation/dto/response/ConnectionResponse.java
@@ -1,8 +1,10 @@
 package com.example.springboot_practice.domain.connection.presentation.dto.response;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class ConnectionResponse {
     private Integer connectionCounts;
     private boolean connected;
