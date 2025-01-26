a36ab71d0101bddf5a2a51935203253a4467c2b2
troy96475 <taesupark0504@gmail.com>
2025-01-25 19:57:56 -0800

    feat :: api추가

--- src/main/java/com/example/springboot_practice/domain/user/presentation/UserController.java
+++ src/main/java/com/example/springboot_practice/domain/user/presentation/UserController.java
@@ -2,9 +2,11 @@ package com.example.springboot_practice.domain.user.presentation;
 
 import com.example.springboot_practice.domain.user.application.DeleteUserService;
 import com.example.springboot_practice.domain.user.application.QueryMyInfoService;
+import com.example.springboot_practice.domain.user.application.QueryOtherUserInfoService;
 import com.example.springboot_practice.domain.user.application.UpdateUserService;
 import com.example.springboot_practice.domain.user.presentation.dto.request.UpdateUserRequest;
 import com.example.springboot_practice.domain.user.presentation.dto.response.MyInfoResponse;
+import com.example.springboot_practice.domain.user.presentation.dto.response.OtherUserInfoResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -16,10 +18,16 @@ public class UserController {
     private final QueryMyInfoService querymyInfoService;
     private final DeleteUserService deleteUserService;
     private final UpdateUserService updateUserService;
+    private final QueryOtherUserInfoService queryOtherUserInfoService;
 
     @GetMapping("/my-info")
     public MyInfoResponse myInfo() {
         return querymyInfoService.QueryMyInfo();
+    }
+
+    @GetMapping("/{accountId}")
+    public OtherUserInfoResponse OtherUserInfo(@PathVariable String accountId) {
+        return queryOtherUserInfoService.queryOtherUserInfo(accountId);
     }
 
     @DeleteMapping("/{accountId}")
@@ -31,4 +39,5 @@ public class UserController {
     public void updateUser(@PathVariable String accountId, @RequestBody @Valid UpdateUserRequest request) {
         updateUserService.updateUser(request, accountId);
     }
+
 }
