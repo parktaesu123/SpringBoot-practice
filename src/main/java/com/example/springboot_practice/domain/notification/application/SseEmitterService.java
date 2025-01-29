b397c96bc3847c81815193b3f9d0ed8a33cc1189
troy96475 <taesupark0504@gmail.com>
2025-01-29 18:14:46 +0900

    feat :: client와 연결

---
+++ src/main/java/com/example/springboot_practice/domain/notification/application/SseEmitterService.java
@@ -0,0 +1,28 @@
+package com.example.springboot_practice.domain.notification.application;
+
+import com.example.springboot_practice.domain.notification.domain.repository.NotificationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+@RequiredArgsConstructor
+public class SseEmitterService {
+    private final Map<Long, SseEmitter> emitters = new ConcurrentHashMap<>();
+    private final NotificationRepository notificationRepository;
+
+    public SseEmitter subscribe(Long userId) {
+        SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
+        emitters.put(userId, emitter);
+
+        emitter.onCompletion(() -> emitters.remove(userId));
+        emitter.onTimeout(() -> emitters.remove(userId));
+        emitter.onError((e) -> emitters.remove(userId));
+
+        return emitter;
+    }
+
+}
