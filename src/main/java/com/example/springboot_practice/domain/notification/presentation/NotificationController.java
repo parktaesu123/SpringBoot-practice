118147b578e3434a59d6ea1617d0bf788843f5ca
troy96475 <taesupark0504@gmail.com>
2025-01-29 18:15:30 +0900

    feat :: sse 통신 연결 api

--- src/main/java/com/example/springboot_practice/domain/notification/presentation/NotificationController.java
+++ src/main/java/com/example/springboot_practice/domain/notification/presentation/NotificationController.java
@@ -1,9 +1,22 @@
 package com.example.springboot_practice.domain.notification.presentation;
 
+import com.example.springboot_practice.domain.notification.application.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 @RequestMapping("/notification")
+@RequiredArgsConstructor
 public class NotificationController {
+    private final NotificationService notificationService;
+
+    @GetMapping("/subscribe")
+    public SseEmitter subscribe(@AuthenticationPrincipal UserDetails userDetails) {
+        return notificationService.subscribe(userDetails);
+    }
 }
