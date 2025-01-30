94f89e71d7deee0bcbb1e18d96f1c8f3f14d5ece
troy96475 <taesupark0504@gmail.com>
2025-01-30 13:16:29 +0900

    feat :: query

--- src/main/java/com/example/springboot_practice/domain/notification/domain/repository/NotificationRepository.java
+++ src/main/java/com/example/springboot_practice/domain/notification/domain/repository/NotificationRepository.java
@@ -1,9 +1,11 @@
 package com.example.springboot_practice.domain.notification.domain.repository;
 
 import com.example.springboot_practice.domain.notification.domain.Notification;
+import com.example.springboot_practice.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    boolean existsByReceiveUser(User receiveUser);
 }
