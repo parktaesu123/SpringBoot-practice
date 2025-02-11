d18a67dc0cf9aae27874d2a4d2a72bfdd70c11f1
troy96475 <taesupark0504@gmail.com>
2025-02-11 19:46:38 +0900

    feat :: heart에 알림 기능 추가

--- src/main/java/com/example/springboot_practice/domain/heart/application/HeartService.java
+++ src/main/java/com/example/springboot_practice/domain/heart/application/HeartService.java
@@ -6,6 +6,10 @@ import com.example.springboot_practice.domain.heart.application.facade.HeartFaca
 import com.example.springboot_practice.domain.heart.domain.Heart;
 import com.example.springboot_practice.domain.heart.domain.repository.HeartRepository;
 import com.example.springboot_practice.domain.heart.presentation.dto.response.HeartResponse;
+import com.example.springboot_practice.domain.notification.application.SendNotificationService;
+import com.example.springboot_practice.domain.notification.application.facade.NotificationFacade;
+import com.example.springboot_practice.domain.notification.domain.enums.NotificationType;
+import com.example.springboot_practice.domain.notification.presentation.dto.NotificationRequest;
 import com.example.springboot_practice.domain.user.application.facade.UserFacade;
 import com.example.springboot_practice.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
@@ -19,11 +23,14 @@ public class HeartService {
     private final FeedFacade feedFacade;
     private final HeartFacade heartFacade;
     private final HeartRepository heartRepository;
+    private final SendNotificationService sendNotificationService;
+    private final NotificationFacade notificationFacade;
 
     @Transactional
     public HeartResponse heartService(String title) {
         User user = userFacade.currentUser();
         Feed feed = feedFacade.getFeeds(title);
+        User feedOwner = feed.getUser();
 
         if (heartFacade.checkHeart(user, feed)) {
             feed.cancelHeart();
@@ -34,11 +41,20 @@ public class HeartService {
                             .user(user)
                             .feed(feed)
                     .build());
+
+            if (!user.equals(feedOwner)) {
+                sendNotificationService.sendNotification(new NotificationRequest(
+                        feedOwner.getUserName(),
+                        user.getUserName() + "님이 하트를 눌렀습니다.",
+                        NotificationType.HEART,
+                        notificationFacade.checkRead(feedOwner)));
+            }
         }
 
         return HeartResponse.builder()
                 .heart(feed.getHeartCounts())
                 .heartStatus(heartFacade.checkHeart(user, feed))
                 .build();
+
     }
 }
