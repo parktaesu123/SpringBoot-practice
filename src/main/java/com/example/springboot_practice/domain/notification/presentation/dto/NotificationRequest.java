c40f7a7bcd7122c24f270f9485fae702894f3085
troy96475 <taesupark0504@gmail.com>
2025-02-11 19:46:10 +0900

    refactor :: notificationRequest

--- src/main/java/com/example/springboot_practice/domain/notification/presentation/dto/NotificationRequest.java
+++ src/main/java/com/example/springboot_practice/domain/notification/presentation/dto/NotificationRequest.java
@@ -2,11 +2,13 @@ package com.example.springboot_practice.domain.notification.presentation.dto;
 
 import com.example.springboot_practice.domain.notification.domain.enums.NotificationType;
 import com.example.springboot_practice.domain.user.domain.User;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class NotificationRequest {
-    private User receiveUser;
+    private String receiveUser;
     private String message;
     private NotificationType type;
     private boolean isRead;
