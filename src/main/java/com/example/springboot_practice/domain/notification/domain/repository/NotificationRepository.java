457b9359a16761e1af93db3f03543e6aff8886ad
troy96475 <taesupark0504@gmail.com>
2025-04-14 16:12:30 +0900

    refactor :: list

--- src/main/java/com/example/springboot_practice/domain/notification/domain/repository/NotificationRepository.java
+++ src/main/java/com/example/springboot_practice/domain/notification/domain/repository/NotificationRepository.java
@@ -1,11 +1,15 @@
 package com.example.springboot_practice.domain.notification.domain.repository;
 
 import com.example.springboot_practice.domain.notification.domain.Notification;
+import com.example.springboot_practice.domain.notification.presentation.dto.response.NotificationResponse;
 import com.example.springboot_practice.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     boolean existsByReceiveUser(User receiveUser);
+
 }
