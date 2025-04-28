d1751f857091b14eea86ab53a87b5ef18e9a1bc8
박태수 <taesupark0504@gmail.com>
2025-04-28 13:50:35 +0900

    refactor :: 알림 조회

--- src/main/java/com/example/springboot_practice/domain/notification/application/QueryMyNotificationService.java
+++ src/main/java/com/example/springboot_practice/domain/notification/application/QueryMyNotificationService.java
@@ -2,17 +2,25 @@ package com.example.springboot_practice.domain.notification.application;
 
 import com.example.springboot_practice.domain.notification.domain.repository.NotificationRepository;
 import com.example.springboot_practice.domain.notification.presentation.dto.response.NotificationResponse;
+import com.example.springboot_practice.domain.user.application.facade.UserFacade;
+import com.example.springboot_practice.domain.user.domain.User;
+import com.example.springboot_practice.domain.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class QueryMyNotificationService {
-    private final NotificationRepository notificationRepository;
+    private final UserRepository userRepository;
+    private final UserFacade userFacade;
 
-    public List<NotificationResponse> queryNotification() {
-        return notificationRepository.findById();
+    @Transactional(readOnly = true)
+    public List<NotificationResponse> queryNotification(String accountId) {
+        User user = userFacade.currentUser();
+
+        return userRepository.findNotificationByAccountId(user.getAccountId());
     }
 }
--- src/main/java/com/example/springboot_practice/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/example/springboot_practice/domain/user/domain/repository/UserRepository.java
@@ -1,12 +1,16 @@
 package com.example.springboot_practice.domain.user.domain.repository;
 
+import com.example.springboot_practice.domain.notification.presentation.dto.response.NotificationResponse;
 import com.example.springboot_practice.domain.user.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByAccountId(String accountId);
 
     void deleteByAccountId(String accountId);
+
+    List<NotificationResponse> findNotificationByAccountId(String accountId);
 }
